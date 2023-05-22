80662ad1f6cc311798fc50344def9eb45af90689
Хуршман Доминика <hristinahursman@iMac-HurshmanDKH.local>
2023-05-22 15:35:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notespro/CreateAccountActivity.java
@@ -0,0 +1,166 @@
+package com.example.notespro;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.rpc.context.AttributeContext;
+
+import java.util.Locale;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText emailEditText,passwordEditText,confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
+    Spinner spinner;
+    public static final String[] languages = {" ", "Русский", "Английский"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+                String selectedLang = parent.getItemAtPosition(position).toString();
+
+                if(selectedLang.equals("Русский")){
+                    setlocal(CreateAccountActivity.this, "rus");
+                    finish();
+                    startActivity(getIntent());
+                }else if (selectedLang.equals("Английский")){
+                    setlocal(CreateAccountActivity.this, "eng");
+                    finish();
+                    startActivity(getIntent());
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        createAccountBtn.setOnClickListener(v-> createAccount());
+        loginBtnTextView.setOnClickListener(v-> finish());
+
+
+    }
+
+
+    public void setlocal (Activity activity, String langCode){
+        Locale locale = new Locale(langCode);
+        Locale.setDefault(locale);
+        Resources resources = activity.getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config,resources.getDisplayMetrics());
+    }
+
+    void createAccount(){
+        String email  = emailEditText.getText().toString();
+        String password  = passwordEditText.getText().toString();
+        String confirmPassword  = confirmPasswordEditText.getText().toString();
+
+        boolean isValidated = validateData(email,password,confirmPassword);
+        if(!isValidated){
+            return;
+        }
+
+        createAccountInFirebase(email,password);
+
+
+    }
+
+    void createAccountInFirebase(String email,String password){
+        changeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(CreateAccountActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        changeInProgress(false);
+                        if(task.isSuccessful()){
+                            //creating acc is done
+                            Utility.showToast(CreateAccountActivity.this, "Successfully create account,Check email to verify");
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+                        }else{
+                            //failure
+                            Utility.showToast(CreateAccountActivity.this,task.getException().getLocalizedMessage());
+                        }
+                    }
+                }
+        );
+
+
+
+    }
+
+    void changeInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            createAccountBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    boolean validateData(String email,String password,String confirmPassword){
+        //validate the data that are input by user.
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if(password.length()<6){
+            passwordEditText.setError("Password length is invalid");
+            return false;
+        }
+        if(!password.equals(confirmPassword)){
+            confirmPasswordEditText.setError("Password not matched");
+            return false;
+        }
+        return true;
+    }
+
+}
